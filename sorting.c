df96770f7e4cd5de130bd2bf3b2b1d35fa57fe5b
cmdoret <cyril.mattheydoret@gmail.com>
2017-01-22 14:49:38 +0100

    changed values to long int to allow larger arrays

--- sorting.c
+++ sorting.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
-void swap(int *arr, int n)
+void swap(long int *arr,long int n)
 {
 
 	if(arr[n]>arr[n+1])
 	{
-		int temp = arr[n];
+		long int temp = arr[n];
 		arr[n] = arr[n+1];
 		arr[n+1] = temp;
 		n--;
@@ -22,18 +23,35 @@ void swap(int *arr, int n)
 int main()
 {
 	printf("Hi, how many numbers do you need to sort ?\n");
-	int nobs;
-	scanf("%d",&nobs);
-	printf("Hello, enter %d numbers and I will sort them.\n",nobs);
-	int nosort[nobs];
-	int i;
-	int c;
-	for(i = 0;i < nobs;i++)
-	{
-		int s;
-		scanf("%d", &s);
-		nosort[i] = s;
+	long int nobs;
+	scanf("%ld",&nobs);
+	printf("What do you want to do ?\n1 - sort randomly generated numbers\n2 - manually enter numbers\n");
+	int usrchoice;
+	scanf("%d",&usrchoice);
+	long int nosort[nobs];
+	long int i;
+	switch(usrchoice){
+		case 1:
+			for(i=0;i<nobs;i++)
+			{
+				nosort[i] = rand() % 100;
+			}
+			printf("unsorted array:\n");
+			for(i=0;i<nobs;i++)
+				printf("%ld; ",nosort[i]);
+			break;
+		case 2:
+			for(i = 0;i < nobs;i++)
+			{
+				long int s;
+				scanf("%ld", &s);
+				nosort[i] = s;
+			}
+			break;
+		default:
+		printf("you had 1 job...");
 	}
+
 	printf("Selection sort...\n");
 	i=0;
 	while(i<nobs-1)
@@ -41,10 +59,11 @@ int main()
 		swap(nosort,i);
 		i+=1;
 	}
+	long int c;
 	printf("Sorted array:\n");
 	for(c=0;c<nobs;c++)
 	{
-		printf("%d;",nosort[c]);
+		printf("%ld; ",nosort[c]);
 	}
 	printf("\n");
 	return 0;

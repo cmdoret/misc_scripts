d18303245469186a327c3d214acc34c944e6c465
cmdoret <cyril.mattheydoret@gmail.com>
2017-05-31 14:59:58 +0200

    added cross validation :)

--- R_linear_model/linear.R
+++ R_linear_model/linear.R
@@ -1,7 +1,7 @@
-# Just writing a linear model from scratch for training.
+# Just writing a linear model from scratch for training with cross validation.
 
 
-lin_mod <- function(df, response){
+train_mod <- function(df, response, exact=T,speed=0.6, plot=F){
   # Takes a dataframe and the name of the variable to predict as input.
   # Returns the input dataframe with the estimated age after minimizing RSS
   
@@ -11,27 +11,60 @@ lin_mod <- function(df, response){
   N <- nrow(df)  # Number of observations
   p <- length(num_features)  # Number of input variables
   X <- as.matrix(df[,num_features])  # Transforming into matrix
-  X <- cbind(rep(1,N),X)  # Adding intercept
+  X <- cbind(rep(1,N),X)  # Adding intercept (bias)
   df[,response] <- as.numeric(df[,response])  # If response is factorial, encoded with integers
   Y <- as.matrix(df[,response])  # Response variable as matrix
   
   # Plotting input-response correlations
-  par(mfrow=c(floor(sqrt(p)),p/floor(sqrt(p))))
-  for(var in num_features){plot(df[,var],df[,response],main=var)}
-  
-  # Solution of the derivative by beta to minimize RSS
-  B_hat <- solve(t(X)%*%X) %*% t(X) %*% Y
-  Y_hat <- X %*% B_hat
-  df_out <- cbind(df, Y_hat)
+  if(plot==T){
+    par(mfrow=c(floor(sqrt(p)),p/floor(sqrt(p))))
+    for(var in num_features){plot(df[,var],df[,response],main=var)}
+  }
+  if(exact==T){
+    # Solution of the derivative by beta to minimize RSS(B)
+    B_hat <- solve(t(X)%*%X) %*% t(X) %*% Y
+    Y_hat <- X %*% B_hat
+    df_out <- cbind(df, Y_hat)
+  } else{
+    
+  }
   return(list(table=df_out,weights=B_hat))
 }
 
+predict_mod <- function(obs, response, weights){
+  # Predicting observations using pre-calculated weights
 
-finish<-lin_mod(iris, 'Species')  # Trying to use linear regression to predict categorical variable
-finish_table<- finish$table
-finish_beta<- finish$weights
+  num_features <- colnames(obs)[unname(sapply(obs,is.numeric))]  # Numeric variables only
+  num_features <- num_features[num_features != response]  # Excluding response variable
+  
+  N <- nrow(obs)  # Number of observations
+  p <- length(num_features)  # Number of input variables
+  X <- as.matrix(obs[,num_features])  # Transforming into matrix
+  X <- cbind(rep(1,N),X)  # Adding intercept (bias)
+  predicted <- X %*% weights
+  return(predicted)
+}
 
-plot(finish_table$Species, finish_table$Y_hat)  # plotting actual species vs predicted species
-abline(h=seq(1.5,3,1))  # Boundaries between species
-1 - length(finish_table$Species[finish_table$Species != round(finish_table$Y_hat)])/nrow(finish_table)  # 97.3% accuracy
-sd(finish_table$Species - finish_table$Y_hat)
+cross_val <- function(data, response, exact=T, speed=0.6){
+  # Cross validation: leave one out
+  
+  init <- rep(NA,nrow(data))  #  initializing df for cross-val. output
+  accuracy <- data.frame(sq_err=init, pred=init)
+  weight_list <- list()
+  for(i in 1:nrow(data)){  # Leave one out (each obs once)
+    trained <- train_mod(data[-i,], response, exact, speed)  # Training without obs i
+    test_obs <- predict_mod(data[i,], response, trained$weights)  # Predicting val i
+    sq_err <- (as.numeric(test_obs) - as.numeric(data[i,response]))^2
+    # computing sqared error from real value
+    weight_list[[i]] <- trained$weights
+    accuracy[i,] <- c(sq_err, test_obs)
+  }
+  return(list(accuracy=accuracy, weights=weight_list))
+}
+
+out_var='Petal.Length'
+results <-cross_val(iris,out_var)
+
+plot(as.numeric(iris[,out_var]),results$accuracy$pred)
+plot(density(results$accuracy$sq_err))
+
